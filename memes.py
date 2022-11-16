4341118bab7db756c0bb53cee03bc126eb6eb4e2
squigglezworth <squigglezworth@gmail.com>
2022-11-15 23:48:34 -0500

    Add memes.py

---
+++ memes.py
@@ -0,0 +1,661 @@
+memes = [
+    {
+        "id": "181913649",
+        "name": "Drake Hotline Bling",
+        "url": "https://i.imgflip.com/30b1gx.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "87743020",
+        "name": "Two Buttons",
+        "url": "https://i.imgflip.com/1g8my4.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "112126428",
+        "name": "Distracted Boyfriend",
+        "url": "https://i.imgflip.com/1ur9b0.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "131087935",
+        "name": "Running Away Balloon",
+        "url": "https://i.imgflip.com/261o3j.jpg",
+        "box_count": 5
+    },
+    {
+        "id": "217743513",
+        "name": "UNO Draw 25 Cards",
+        "url": "https://i.imgflip.com/3lmzyx.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "124822590",
+        "name": "Left Exit 12 Off Ramp",
+        "url": "https://i.imgflip.com/22bdq6.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "247375501",
+        "name": "Buff Doge vs. Cheems",
+        "url": "https://i.imgflip.com/43a45p.png",
+        "box_count": 4
+    },
+    {
+        "id": "222403160",
+        "name": "Bernie I Am Once Again Asking For Your Support",
+        "url": "https://i.imgflip.com/3oevdk.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "129242436",
+        "name": "Change My Mind",
+        "url": "https://i.imgflip.com/24y43o.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "131940431",
+        "name": "Gru's Plan",
+        "url": "https://i.imgflip.com/26jxvz.jpg",
+        "box_count": 4
+    },
+    {
+        "id": "4087833",
+        "name": "Waiting Skeleton",
+        "url": "https://i.imgflip.com/2fm6x.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "438680",
+        "name": "Batman Slapping Robin",
+        "url": "https://i.imgflip.com/9ehk.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "135256802",
+        "name": "Epic Handshake",
+        "url": "https://i.imgflip.com/28j0te.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "97984",
+        "name": "Disaster Girl",
+        "url": "https://i.imgflip.com/23ls.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "93895088",
+        "name": "Expanding Brain",
+        "url": "https://i.imgflip.com/1jwhww.jpg",
+        "box_count": 4
+    },
+    {
+        "id": "188390779",
+        "name": "Woman Yelling At Cat",
+        "url": "https://i.imgflip.com/345v97.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "252600902",
+        "name": "Always Has Been",
+        "url": "https://i.imgflip.com/46e43q.png",
+        "box_count": 2
+    },
+    {
+        "id": "80707627",
+        "name": "Sad Pablo Escobar",
+        "url": "https://i.imgflip.com/1c1uej.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "1035805",
+        "name": "Boardroom Meeting Suggestion",
+        "url": "https://i.imgflip.com/m78d.jpg",
+        "box_count": 4
+    },
+    {
+        "id": "178591752",
+        "name": "Tuxedo Winnie The Pooh",
+        "url": "https://i.imgflip.com/2ybua0.png",
+        "box_count": 2
+    },
+    {
+        "id": "61579",
+        "name": "One Does Not Simply",
+        "url": "https://i.imgflip.com/1bij.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "119139145",
+        "name": "Blank Nut Button",
+        "url": "https://i.imgflip.com/1yxkcp.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "110163934",
+        "name": "I Bet He's Thinking About Other Women",
+        "url": "https://i.imgflip.com/1tl71a.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "27813981",
+        "name": "Hide the Pain Harold",
+        "url": "https://i.imgflip.com/gk5el.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "91538330",
+        "name": "X, X Everywhere",
+        "url": "https://i.imgflip.com/1ihzfe.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "102156234",
+        "name": "Mocking Spongebob",
+        "url": "https://i.imgflip.com/1otk96.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "148909805",
+        "name": "Monkey Puppet",
+        "url": "https://i.imgflip.com/2gnnjh.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "180190441",
+        "name": "They're The Same Picture",
+        "url": "https://i.imgflip.com/2za3u1.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "161865971",
+        "name": "Marked Safe From",
+        "url": "https://i.imgflip.com/2odckz.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "195515965",
+        "name": "Clown Applying Makeup",
+        "url": "https://i.imgflip.com/38el31.jpg",
+        "box_count": 4
+    },
+    {
+        "id": "226297822",
+        "name": "Panik Kalm Panik",
+        "url": "https://i.imgflip.com/3qqcim.png",
+        "box_count": 3
+    },
+    {
+        "id": "79132341",
+        "name": "Bike Fall",
+        "url": "https://i.imgflip.com/1b42wl.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "216951317",
+        "name": "Guy Holding Cardboard Sign",
+        "url": "https://i.imgflip.com/3l60ph.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "101470",
+        "name": "Ancient Aliens",
+        "url": "https://i.imgflip.com/26am.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "55311130",
+        "name": "This Is Fine",
+        "url": "https://i.imgflip.com/wxica.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "114585149",
+        "name": "Inhaling Seagull",
+        "url": "https://i.imgflip.com/1w7ygt.jpg",
+        "box_count": 4
+    },
+    {
+        "id": "100777631",
+        "name": "Is This A Pigeon",
+        "url": "https://i.imgflip.com/1o00in.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "123999232",
+        "name": "The Scroll Of Truth",
+        "url": "https://i.imgflip.com/21tqf4.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "3218037",
+        "name": "This Is Where I'd Put My Trophy If I Had One",
+        "url": "https://i.imgflip.com/1wz1x.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "155067746",
+        "name": "Surprised Pikachu",
+        "url": "https://i.imgflip.com/2kbn1e.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "89370399",
+        "name": "Roll Safe Think About It",
+        "url": "https://i.imgflip.com/1h7in3.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "99683372",
+        "name": "Sleeping Shaq",
+        "url": "https://i.imgflip.com/1nck6k.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "135678846",
+        "name": "Who Killed Hannibal",
+        "url": "https://i.imgflip.com/28s2gu.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "28251713",
+        "name": "Oprah You Get A",
+        "url": "https://i.imgflip.com/gtj5t.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "124055727",
+        "name": "Y'all Got Any More Of That",
+        "url": "https://i.imgflip.com/21uy0f.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "134797956",
+        "name": "American Chopper Argument",
+        "url": "https://i.imgflip.com/2896ro.jpg",
+        "box_count": 5
+    },
+    {
+        "id": "61556",
+        "name": "Grandma Finds The Internet",
+        "url": "https://i.imgflip.com/1bhw.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "21735",
+        "name": "The Rock Driving",
+        "url": "https://i.imgflip.com/grr.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "84341851",
+        "name": "Evil Kermit",
+        "url": "https://i.imgflip.com/1e7ql7.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "5496396",
+        "name": "Leonardo Dicaprio Cheers",
+        "url": "https://i.imgflip.com/39t1o.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "61520",
+        "name": "Futurama Fry",
+        "url": "https://i.imgflip.com/1bgw.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "101288",
+        "name": "Third World Skeptical Kid",
+        "url": "https://i.imgflip.com/265k.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "196652226",
+        "name": "Spongebob Ight Imma Head Out",
+        "url": "https://i.imgflip.com/392xtu.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "259237855",
+        "name": "Laughing Leo",
+        "url": "https://i.imgflip.com/4acd7j.png",
+        "box_count": 2
+    },
+    {
+        "id": "175540452",
+        "name": "Unsettled Tom",
+        "url": "https://i.imgflip.com/2wifvo.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "132769734",
+        "name": "Hard To Swallow Pills",
+        "url": "https://i.imgflip.com/271ps6.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "6235864",
+        "name": "Finding Neverland",
+        "url": "https://i.imgflip.com/3pnmg.jpg",
+        "box_count": 3
+    },
+    {
+        "id": "91545132",
+        "name": "Trump Bill Signing",
+        "url": "https://i.imgflip.com/1ii4oc.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "8072285",
+        "name": "Doge",
+        "url": "https://i.imgflip.com/4t0m5.jpg",
+        "box_count": 5
+    },
+    {
+        "id": "17496002",
+        "name": "Leonardo Dicaprio Wolf Of Wall Street",
+        "url": "https://i.imgflip.com/af002.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "61544",
+        "name": "Success Kid",
+        "url": "https://i.imgflip.com/1bhk.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "61546",
+        "name": "Brace Yourselves X is Coming",
+        "url": "https://i.imgflip.com/1bhm.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "29617627",
+        "name": "Look At Me",
+        "url": "https://i.imgflip.com/hmt3v.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "61532",
+        "name": "The Most Interesting Man In The World",
+        "url": "https://i.imgflip.com/1bh8.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "14371066",
+        "name": "Star Wars Yoda",
+        "url": "https://i.imgflip.com/8k0sa.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "24557067",
+        "name": "Afraid To Ask Andy",
+        "url": "https://i.imgflip.com/emccr.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "563423",
+        "name": "That Would Be Great",
+        "url": "https://i.imgflip.com/c2qn.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "163573",
+        "name": "Imagination Spongebob",
+        "url": "https://i.imgflip.com/3i7p.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "29562797",
+        "name": "I'm The Captain Now",
+        "url": "https://i.imgflip.com/hlmst.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "89655",
+        "name": "Uncle Sam",
+        "url": "https://i.imgflip.com/1x6f.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "101716",
+        "name": "Yo Dawg Heard You",
+        "url": "https://i.imgflip.com/26hg.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "61539",
+        "name": "First World Problems",
+        "url": "https://i.imgflip.com/1bhf.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "460541",
+        "name": "Jack Sparrow Being Chased",
+        "url": "https://i.imgflip.com/9vct.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "1367068",
+        "name": "I Should Buy A Boat Cat",
+        "url": "https://i.imgflip.com/tau4.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "8279814",
+        "name": "Cute Cat",
+        "url": "https://i.imgflip.com/4xgqu.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "6531067",
+        "name": "See Nobody Cares",
+        "url": "https://i.imgflip.com/3vzej.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "61585",
+        "name": "Bad Luck Brian",
+        "url": "https://i.imgflip.com/1bip.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "16464531",
+        "name": "But That's None Of My Business",
+        "url": "https://i.imgflip.com/9sw43.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "4173692",
+        "name": "Scared Cat",
+        "url": "https://i.imgflip.com/2hgfw.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "405658",
+        "name": "Grumpy Cat",
+        "url": "https://i.imgflip.com/8p0a.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "61533",
+        "name": "X All The Y",
+        "url": "https://i.imgflip.com/1bh9.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "101910402",
+        "name": "Who Would Win?",
+        "url": "https://i.imgflip.com/1ooaki.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "142921050",
+        "name": "Car Salesman Slaps Roof Of Car",
+        "url": "https://i.imgflip.com/2d3al6.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "21604248",
+        "name": "Mugatu So Hot Right Now",
+        "url": "https://i.imgflip.com/cv1y0.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "7183956",
+        "name": "Oprah You Get A Car Everybody Gets A Car",
+        "url": "https://i.imgflip.com/49z6c.jpg",
+        "box_count": 4
+    },
+    {
+        "id": "101511",
+        "name": "Don't You Squidward",
+        "url": "https://i.imgflip.com/26br.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "61580",
+        "name": "Too Damn High",
+        "url": "https://i.imgflip.com/1bik.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "444501",
+        "name": "Maury Lie Detector",
+        "url": "https://i.imgflip.com/9iz9.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "61527",
+        "name": "Y U No",
+        "url": "https://i.imgflip.com/1bh3.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "176908",
+        "name": "Shut Up And Take My Money Fry",
+        "url": "https://i.imgflip.com/3si4.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "1202623",
+        "name": "Keep Calm And Carry On Red",
+        "url": "https://i.imgflip.com/pry7.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "61582",
+        "name": "Creepy Condescending Wonka",
+        "url": "https://i.imgflip.com/1bim.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "922147",
+        "name": "Laughing Men In Suits",
+        "url": "https://i.imgflip.com/jrj7.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "13036679",
+        "name": "Heavy Breathing Cat",
+        "url": "https://i.imgflip.com/7rf5z.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "183518946",
+        "name": "Blank Transparent Square",
+        "url": "https://i.imgflip.com/319g4i.png",
+        "box_count": 2
+    },
+    {
+        "id": "371382",
+        "name": "Simba Shadowy Place",
+        "url": "https://i.imgflip.com/7yk6.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "1464444",
+        "name": "Happy Star Congratulations",
+        "url": "https://i.imgflip.com/vdz0.jpg",
+        "box_count": 4
+    },
+    {
+        "id": "61516",
+        "name": "Philosoraptor",
+        "url": "https://i.imgflip.com/1bgs.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "1232104",
+        "name": "Pepperidge Farm Remembers",
+        "url": "https://i.imgflip.com/qep4.jpg",
+        "box_count": 2
+    },
+    {
+        "id": "56225174",
+        "name": "Be Like Bill",
+        "url": "https://i.imgflip.com/xh3me.jpg",
+        "box_count": 4
+    }
+]
+
+import discord
+from discord.ext import commands
+from random import randint
+
+def chunk(l, c):
+    # Split a list into chunks
+    for i in range(0, len(l), c):
+        yield l[i:i + c]
+
+class Memes(commands.Cog):
+    async def CommandCallback(self, ctx):
+        print(f"[memes] Responding to {ctx.user}")
+
+        embed = discord.Embed(color=0x299aff, description=RandomMeme["name"])
+
+        RandomMeme = self.memes[randint(1, len(self.memes))]
+        embed.set_image(url=RandomMeme["url"])
+
+        view = self.BuildView(self)
+
+        await ctx.respond(ephemeral=True, embed=embed, view=view)
+
+    def __init__(self, bot, guilds = None):
+        self.bot = bot
+        global memes
+        self.memes = memes
+
+        print(f"[memes] Registering /memes" + (f" on {len(guilds)} guilds" if guilds else " globally"))
+
+        command = discord.SlashCommand(self.CommandCallback, name="memes", description="shitpost")
+        bot.add_application_command(command)
+
+    class BuildDropdown(discord.ui.Select):
+        def __init__(self, memes, index):
+            self.memes = memes
+
+            options = []
+            for m in memes:
+                options += [discord.SelectOption(
+                    label=m,
+                    value=m
+                )]
+
+            super().__init__(
+                placeholder=f"Meme Menu {index}",
+                options=options,
+                min_values=1)
+
+    class BuildView(discord.ui.View):
+        def __init__(self, cog):
+            super().__init__()
+
+            self.cog = cog
+            MemeNames = [m["name"] for m in self.cog.memes]
+            i = 0
+            for memes in chunk(MemeNames, 25):
+                i += 1
+                self.add_item(cog.BuildDropdown(memes, i))
