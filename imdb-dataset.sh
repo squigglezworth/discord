5a17c51e602c95b66f7b457628165dd81c714dad
squigglezworth <squigglezworth@gmail.com>
2022-12-18 15:48:46 -0500

    Update imdb-dataset script

--- imdb-dataset.sh
+++ imdb-dataset.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# This script downloads the IMDb dataset archives and imports them to imdb.sqlite
-# Note: This process takes up 10-15GB and will take a while
+# Example script showing how to download the IMDb dataset archives and import them to an sqlite db
+# Seeding the db will take an hour or more
+# Takes up 10-15GB (2x that if you use a tmp db as this script does, to reduce downtime)
 
-wget -N -P /tmp/imdb-datasets/ \
+wget -N -P "$@/imdb-dataset/" \
 "https://datasets.imdbws.com/name.basics.tsv.gz" \
 "https://datasets.imdbws.com/title.akas.tsv.gz" \
 "https://datasets.imdbws.com/title.basics.tsv.gz" \
@@ -12,10 +13,12 @@ wget -N -P /tmp/imdb-datasets/ \
 "https://datasets.imdbws.com/title.principals.tsv.gz" \
 "https://datasets.imdbws.com/title.ratings.tsv.gz"
 
-wget -N https://raw.githubusercontent.com/squigglezworth/cinemagoer/master/bin/s32cinemagoer.py
-
-s32cinemagoer.py --cleanup /tmp/imdb-datasets/ sqlite:///imdb.tmp.sqlite
-
-mv imdb.tmp.sqlite imdb.sqlite
-
-rm -rf /tmp/imdb-datasets/
+wget -N https://raw.githubusercontent.com/squigglezworth/cinemagoer/master/bin/s32cinemagoer.py \
+&& \
+python3 s32cinemagoer.py --verbose --cleanup "$@/imdb-dataset/" "sqlite:///$@/imdb.tmp.sqlite" \
+&& \
+mv "$@/imdb.tmp.sqlite" "$@/imdb.sqlite" \
+&& \
+ln -s "$@/imdb.sqlite" "imdb.sqlite" \
+&& \
+rm -rf "$@/imdb-dataset/"
