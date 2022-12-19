d0e80414a474fb14e5f06122ab24bb7af961b986
squigglezworth <squigglezworth@gmail.com>
2022-12-18 22:34:02 -0500

    publisher.py - Stop using hardcoded db

--- cogs/publisher.py
+++ cogs/publisher.py
@@ -22,11 +22,11 @@ EMOJI = [
 ]
 
 
-def remove_from_db(guild, channels):
+def remove_from_db(db, guild, channels):
     """
     Utility function to remove channels from the current guilds tracked channels
     """
-    with SqliteDict("publisher.sqlite", tablename=str(guild.id)) as db:
+    with SqliteDict(db, tablename=str(guild.id)) as db:
         if guild.id in db:
             db[guild.id] = [c for c in db[guild.id] if c not in channels]
 
@@ -35,11 +35,11 @@ def remove_from_db(guild, channels):
         return db[guild.id]
 
 
-def add_to_db(guild, channels):
+def add_to_db(db, guild, channels):
     """
     Utility function to add channels to the current guilds tracked channels
     """
-    with SqliteDict("publisher.sqlite", tablename=str(guild.id)) as db:
+    with SqliteDict(db, tablename=str(guild.id)) as db:
         if guild.id in db:
             db[guild.id] += channels
         else:
@@ -53,10 +53,11 @@ def add_to_db(guild, channels):
 class AutoPublisher(commands.Cog):
     settings = []
 
-    def __init__(self, bot):
+    def __init__(self, bot, db):
         bot.logger.info(f"Registering /publisher")
 
-        self.logger = logging.getLogger("bot.publisher")
+        self.logger = bot.logger
+        self.db = db
         self.bot = bot
 
     @commands.slash_command()
@@ -67,7 +68,7 @@ class AutoPublisher(commands.Cog):
         if not ctx.channel.permissions_for(ctx.user).manage_channels:
             return await ctx.respond("*You need the **Manage Channels** permission to use this command*", ephemeral=True)
 
-        with SqliteDict("publisher.sqlite", tablename=str(ctx.guild.id)) as db:
+        with SqliteDict(self.db, tablename=str(ctx.guild.id)) as db:
             if ctx.guild.id in db:
                 self.settings = db[ctx.guild.id]
 
@@ -84,19 +85,19 @@ class AutoPublisher(commands.Cog):
             return
 
         if not self.settings:
-            with SqliteDict("publisher.sqlite", tablename=str(message.guild.id)) as db:
+            with SqliteDict(self.db, tablename=str(message.guild.id)) as db:
                 if message.guild.id in db:
                     self.settings = db[message.guild.id]
 
         if message.channel.id in self.settings:
-            self.logger.info(f"Publishing message from '{message.channel.name}' on '{message.guild.name}'")
+            self.logger.info(f"publisher - Publishing message from '#{message.channel.name}' on '{message.guild.name}'")
 
             try:
                 await message.publish()
             except discord.Forbidden:
-                self.logger.warning(f"Missing permissions for '{message.channel.name}' on '{message.guild.name}'")
+                self.logger.warning(f"publisher - Missing permissions for '#{message.channel.name}' on '{message.guild.name}'")
             except dicsord.HTTPException:
-                self.logger.warning(f"Publishing failed for '{message.channel.name}' on '{message.guild.name}'")
+                self.logger.warning(f"publisher - Publishing failed for '#{message.channel.name}' on '{message.guild.name}'")
 
     def Message(self, ctx):
         """
@@ -152,15 +153,16 @@ class View(discord.ui.View):
         self.cog = cog
         self.ctx = ctx
         self.channels = cog.channels
+        self.db = cog.db
 
         for i, c in enumerate(self.channels):
             self.add_item(ChannelButton(i + 1, ctx, cog))
 
     @discord.ui.button(label="Enable for All", style=discord.ButtonStyle.success)
     async def EnableButton(self, button, interaction):
-        self.cog.logger.info(f"Enabling auto-publishing for all channels on '{self.ctx.guild.name}'")
+        self.cog.logger.info(f"publisher - Enabling auto-publishing for all channels on '{self.ctx.guild.name}'")
 
-        settings = add_to_db(self.ctx.guild, [c[0].id for c in self.channels])
+        settings = add_to_db(self.db, self.ctx.guild, [c[0].id for c in self.channels])
         self.cog.settings = settings
 
         message = self.cog.Message(self.ctx)
@@ -168,9 +170,9 @@ class View(discord.ui.View):
 
     @discord.ui.button(label="Disable for All", style=discord.ButtonStyle.red)
     async def DisableButton(self, button, interaction):
-        self.cog.logger.info(f"Disabling auto-publishing for all channels on '{self.ctx.guild.name}'")
+        self.cog.logger.info(f"publisher - Disabling auto-publishing for all channels on '{self.ctx.guild.name}'")
 
-        settings = remove_from_db(self.ctx.guild, [c[0].id for c in self.channels])
+        settings = remove_from_db(self.db, self.ctx.guild, [c[0].id for c in self.channels])
         self.cog.settings = settings
 
         message = self.cog.Message(self.ctx)
@@ -185,6 +187,7 @@ class ChannelButton(discord.ui.Button):
     def __init__(self, index, ctx, cog):
         self.ctx = ctx
         self.cog = cog
+        self.db = cog.db
 
         channel = cog.channels[index - 1][0]
         style = discord.ButtonStyle.green
@@ -195,13 +198,13 @@ class ChannelButton(discord.ui.Button):
 
     async def callback(self, interaction):
         channel = self.cog.channels[int(interaction.custom_id) - 1][0]
-        self.cog.logger.info(f"Toggling publishing for '{channel.name}' on '{channel.guild.name}'")
+        self.cog.logger.info(f"publisher - Toggling publishing for '{channel.name}' on '{channel.guild.name}'")
 
         if channel.id in self.cog.settings:
-            settings = remove_from_db(interaction.guild, [channel.id])
+            settings = remove_from_db(self.db, interaction.guild, [channel.id])
             self.cog.settings = settings
         else:
-            settings = add_to_db(interaction.guild, [channel.id])
+            settings = add_to_db(self.db, interaction.guild, [channel.id])
             self.cog.settings = settings
 
         message = self.cog.Message(self.ctx)
