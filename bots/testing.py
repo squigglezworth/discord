7d802e79632166b33c7bf71dc58af27f506360fd
squigglezworth <squigglezworth@gmail.com>
2022-12-19 15:58:43 -0500

    utils/webhook - Send an embed instead of a plain message

--- bots/testing.py
+++ bots/testing.py
@@ -2,7 +2,7 @@ import bot
 import discord
 import logging
 import os
-from lib.webhook import WebhookHandler
+from utils.webhook import WebhookHandler
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -15,16 +15,17 @@ GUILDS = [int(g) for g in os.getenv("GUILDS").split(",")] if os.getenv("GUILDS")
 
 intents = discord.Intents.default()
 intents.guilds = True
-bot = bot.Bot("hideaway-debug", log_webhook="", intents=intents, debug_guilds=GUILDS)
+bot = bot.Bot("hideaway-debug", intents=intents, debug_guilds=GUILDS)
 
 # Setup webhook logging
 log_webhook = os.getenv("LOG_WEBHOOK")
 if log_webhook:
-    webhook_handler = WebhookHandler(log_webhook)
+    handler = WebhookHandler(log_webhook)
     fmt = logging.Formatter(f"%(name)s - %(levelname)s - %(message)s")
     fmt.default_msec_format = None
-    webhook_handler.setFormatter(fmt)
-    bot.logger.addHandler(webhook_handler)
+    handler.setFormatter(fmt)
+    handler.setLevel(logging.WARNING)
+    bot.logger.addHandler(handler)
 
 
 @commands.slash_command()
--- utils/webhook.py
+++ utils/webhook.py
@@ -1,27 +1,26 @@
 # A very simple implementation of a logging handler that posts to a (Discord) webhook
 import logging
+import requests
 from logging.handlers import HTTPHandler
 
 
 class WebhookHandler(logging.Handler):
-    def __init__(self, webhook):
-        import requests
-
+    def __init__(self, webhook, color=0x0299AFF):
         logging.Handler.__init__(self)
 
         self.url = webhook
-        self.session = requests.Session()
+        self.color = color
 
     def mapLogRecord(self, record):
         record_modified = HTTPHandler.mapLogRecord(self, record)
         try:
-            record_modified["content"] = f"`{self.format(record)}`"
+            record_modified["data"] = {"embeds": [{"color": self.color, "description": f"```{self.format(record)}```"}]}
         except:
             pass
         return record_modified
 
     def emit(self, record):
         try:
-            self.session.post(self.url, json=self.mapLogRecord(record), timeout=10)
+            requests.post(self.url, json=self.mapLogRecord(record)["data"], timeout=10)
         except:
             self.handleError(record)
