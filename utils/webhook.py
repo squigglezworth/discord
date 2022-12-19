978478301d1cfc97835827b238f202ec02480566
squigglezworth <squigglezworth@gmail.com>
2022-12-19 18:47:23 -0500

    webhook - Minor refactoring

--- utils/webhook.py
+++ utils/webhook.py
@@ -13,14 +13,13 @@ class WebhookHandler(logging.Handler):
 
     def mapLogRecord(self, record):
         record_modified = HTTPHandler.mapLogRecord(self, record)
-        try:
-            record_modified["data"] = {"embeds": [{"color": self.color, "description": f"```{self.format(record)}```"}]}
-        except:
-            pass
-        return record_modified
+        # record_modified schema: name, msg, args, levelname, levelno, pathname, filename, module, exc_info, exc_text, stack_info, lineno, funcName, created, msecs, relativeCreated, thread, threadName, processName, process, message, asctime
+        # self.format returns the actual log message
+        webhook_data = {"embeds": [{"color": self.color, "description": f"```{self.format(record)}```"}]}
+        return webhook_data
 
     def emit(self, record):
         try:
-            requests.post(self.url, json=self.mapLogRecord(record)["data"], timeout=10)
+            requests.post(self.url, json=self.mapLogRecord(record))
         except:
             self.handleError(record)
